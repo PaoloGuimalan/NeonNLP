46f9c56ec090b8d7254bc7d225a95ddb9037bc13
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2023-11-01 16:31:22 +0800

    formatized and organized types in single object of a word

--- index.py
+++ index.py
@@ -1,19 +1,45 @@
 import sys
 import re
 import json
+import pprint
 
 rawphrase = sys.argv[1]
 wrd = sys.argv[1]
+typesDefault = {
+    "NOUN": "n.",
+    "PRONOUN": "p.",
+    "VERB": "v.",
+    "ADJECTIVE": "adj.",
+    "ADVERB": "adv.",
+    "PREPOSITION": "prep.",
+    "CONJUNCTION": "conj.",
+    "INTERJECTION": "i."
+}
 
 def fetchWordFiltered(wrdprop):
     nlpdictionary = open("./resources/nlp_dictionary.json")
     data = json.load(nlpdictionary)
     filteredArray = [x for x in data if x['word'] == wrdprop]
     
-    return [{
-        "word": element["word"],
-        "type": element["type"]
-    } for element in filteredArray]
+    formattedWordHolder = wrdprop
+    formattedWordObject = []
+    
+    for element in filteredArray:
+        formattedWordHolder = element["word"]
+        
+        if element["type"] != "()":
+            if element["type"] not in formattedWordObject:
+                formattedWordObject.append(element["type"])
+    
+    # return [{
+    #     "word": element["word"],
+    #     "type": element["type"]
+    # } for element in filteredArray]
+    
+    return {
+        "word": formattedWordHolder,
+        "type": formattedWordObject
+    }
 
 def tokenizeSentences(sentence):
     seekDefArray = []
@@ -30,6 +56,7 @@ def tokenizeSentences(sentence):
 
 def tokenization(data):
     finalArray = []
+    cleanArray = []
     
     tokenizedToSentences = re.split('\\.|\\?|\\!', data)
     tokenizedToSentencesClean = list(filter(None, tokenizedToSentences))
@@ -37,8 +64,23 @@ def tokenization(data):
         tokenSntcsHolder = tokenizeSentences(stncs)
         finalArray.append(tokenSntcsHolder)
         
+    for fnlarr in finalArray:
+        print(fnlarr)
+        # cleanArray.append()
+        
     print(finalArray)
     
 tokenization(rawphrase)
     
 # fetchWordFiltered(wrd)
+
+def getWordTypes():
+    nlpdictionary = open("./resources/nlp_dictionary.json")
+    data = json.load(nlpdictionary)
+    filteredDuplicates = []
+    typesArray = [typex["type"] for typex in data]
+    [filteredDuplicates.append(x) for x in typesArray if x not in filteredDuplicates]
+    
+    print(filteredDuplicates)
+    
+# getWordTypes()
